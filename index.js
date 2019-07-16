fb0d744ccc3ad99c89c1a41e3eba0501bebe43ef
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-16 23:56:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(range) {
   return function drvingRange(str1, str2) {
-    let dist = range - parseInt(str2) - parseInt(str1);
-    console.log(dist > 0 ? `positive` : `negative`)
+    const dist = range - parseInt(str2) - parseInt(str1);
+    return dist > 0 ? `within range by ${dist}` : `${dist} blocks out of range`
   }
 }
