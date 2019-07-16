6feae4e028d83dce34142f3bdadc2fb70d4e9064
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-16 23:54:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(range) {
   return function drvingRange(str1, str2) {
-    const dist = parseInt(str2) - parseInt(str1) - range;
+    let dist = parseInt(str2) - parseInt(str1) - range;
     console.log(dist > 0 ? `positive` : `negative`)
   }
 }
